24b385143547dd778c77492542e21d88c61193f1
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-06-24 17:03:13 +0500

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -667,3 +667,18 @@ function order_now_value(objButton, packid=1){
         sidebar.style.width = '0px';
     }
 }
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
--- components/portfolio.php
+++ components/portfolio.php
@@ -68,3 +68,162 @@
       </div>
   </section>
 </section>
+
+
+
+
+<!--  -->
+<section class="portfolio-tabs">
+    <div class="container-fluid" >
+        <div class="row">
+    <div class="text-center">
+        <h2 class="title-heading">Check Out Our Most Recent Designs</h2>
+        <p class="title-p">Browse through our latest design projects, demonstrating our commitment to creativity,<br>  innovation, and client satisfaction.</p>   
+        </div>
+    <ul class="tabnav-projects">
+            <li data-targetit="box-port1" class="active first"><a href="javascript:;">All</a></li>
+            <li data-targetit="box-port2"><a href="javascript:;">Branding</a></li>
+            <li data-targetit="box-port3"><a href="javascript:;">Logo Design</a></li>
+            <li data-targetit="box-port4"><a href="javascript:;">Social Media</a></li>
+            <li data-targetit="box-port5"><a href="javascript:;">Web Design and Development</a></li>
+            <li data-targetit="box-port6"><a href="javascript:;">Animation Video</a></li>
+            <!-- <li data-targetit="box-port7"><a href="javascript:;">Brand Marketing</a></li> -->
+        </ul>
+    <div class="box-port1 showfirst">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery">
+                        <img src="./assets/images/portfolio/branding/b1.png" alt="a forest after an apocalypse">
+                        <img src="./assets/images/portfolio/v2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/web/w2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/branding/b2.png" alt="a waterfall and many rocks">
+                        <img src="./assets/images/portfolio/l2.png" alt="sime pink flowers">
+                        <img src="./assets/images/portfolio/social-media/s2.png" alt="a cool landscape">
+                        <img src="./assets/images/portfolio/animated/sc7.webp" data-video="assets/images/portfolio/animated/video7.mp4" alt="Video 7">
+                        <img src="./assets/images/portfolio/web/w1.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/l1.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/social-media/s1.png" alt="a waterfall, a lot of tree and a great view from the sky">
+                        <img src="./assets/images/portfolio/l3.png" alt="big rocks with some trees">
+                        <img src="./assets/images/portfolio/animated/sc12.webp" data-video="assets/images/portfolio/animated/video12.mp4" alt="Video 12">
+                      </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="box-port2">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery">
+                        <img src="./assets/images/portfolio/branding/b1.png" alt="a forest after an apocalypse">
+                        <img src="./assets/images/portfolio/branding/b2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/branding/b3.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/branding/b4.png" alt="a waterfall and many rocks">
+                        <img src="./assets/images/portfolio/branding/b5.png" alt="sime pink flowers">
+                        <img src="./assets/images/portfolio/branding/b6.png" alt="a cool landscape">
+                        <img src="./assets/images/portfolio/branding/b7.png" alt="inside a town between two big buildings">
+                        <img src="./assets/images/portfolio/branding/b8.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/branding/b9.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/branding/b10.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/branding/b11.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/branding/b12.png" alt="a house on a mountain">
+                    </div>
+                </div>
+        </ul>
+    </div>
+
+    <div class="box-port3">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery">
+                        <img src="./assets/images/portfolio/logo/l1.png" alt="a forest after an apocalypse">
+                        <img src="./assets/images/portfolio/logo/l2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/logo/l3.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/logo/l4.png" alt="a waterfall and many rocks">
+                        <img src="./assets/images/portfolio/logo/l5.png" alt="sime pink flowers">
+                        <img src="./assets/images/portfolio/logo/l6.png" alt="a cool landscape">
+                        <img src="./assets/images/portfolio/logo/l7.png" alt="inside a town between two big buildings">
+                        <img src="./assets/images/portfolio/logo/l8.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/logo/l9.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/logo/l10.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/logo/l11.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/logo/l12.png" alt="a house on a mountain">
+                      </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="box-port4">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery">
+                        <img src="./assets/images/portfolio/social-media/s1.png" alt="a forest after an apocalypse">
+                        <img src="./assets/images/portfolio/social-media/s2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/social-media/s3.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/social-media/s4.png" alt="a waterfall and many rocks">
+                        <img src="./assets/images/portfolio/social-media/s5.png" alt="sime pink flowers">
+                        <img src="./assets/images/portfolio/social-media/s6.png" alt="a cool landscape">
+                        <img src="./assets/images/portfolio/social-media/s7.png" alt="inside a town between two big buildings">
+                        <img src="./assets/images/portfolio/social-media/s8.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/social-media/s9.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/social-media/s10.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/social-media/s11.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/social-media/s12.png" alt="a house on a mountain">
+                      </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="box-port5">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery">
+                        <img src="./assets/images/portfolio/web/w1.png" alt="a forest after an apocalypse">
+                        <img src="./assets/images/portfolio/web/w2.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/web/w3.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/web/w4.png" alt="a waterfall and many rocks">
+                        <img src="./assets/images/portfolio/web/w5.png" alt="sime pink flowers">
+                        <img src="./assets/images/portfolio/web/w6.png" alt="a cool landscape">
+                        <img src="./assets/images/portfolio/web/w7.png" alt="inside a town between two big buildings">
+                        <img src="./assets/images/portfolio/web/w8.png" alt="a great view of the sea above the mountain">
+                        <img src="./assets/images/portfolio/web/w9.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/web/w10.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/web/w11.png" alt="a house on a mountain">
+                        <img src="./assets/images/portfolio/web/w12.png" alt="a house on a mountain">
+                      </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="box-port6">
+        <ul class="portfolio-items">
+            <li>
+                <div class="portfolio-items-box">
+                    <div class="gallery1">
+                    <img src="./assets/images/portfolio/animated/sc1.webp" data-video="assets/images/portfolio/animated/video1.mp4" alt="Video 1">
+                    <img src="./assets/images/portfolio/animated/sc2.webp" data-video="assets/images/portfolio/animated/video2.mp4" alt="Video 2">
+                    <img src="./assets/images/portfolio/animated/sc3.webp" data-video="assets/images/portfolio/animated/video3.mp4" alt="Video 3">
+                    <img src="./assets/images/portfolio/animated/sc4.webp" data-video="assets/images/portfolio/animated/video4.mp4" alt="Video 4">
+                    <img src="./assets/images/portfolio/animated/sc5.webp" data-video="assets/images/portfolio/animated/video5.mp4" alt="Video 5">
+                    <img src="./assets/images/portfolio/animated/sc6.webp" data-video="assets/images/portfolio/animated/video6.mp4" alt="Video 6">
+                    <img src="./assets/images/portfolio/animated/sc7.webp" data-video="assets/images/portfolio/animated/video7.mp4" alt="Video 7">
+                    <img src="./assets/images/portfolio/animated/sc8.webp" data-video="assets/images/portfolio/animated/video8.mp4" alt="Video 8">
+                    <img src="./assets/images/portfolio/animated/sc9.webp" data-video="assets/images/portfolio/animated/video9.mp4" alt="Video 9">
+                    <img src="./assets/images/portfolio/animated/sc10.webp" data-video="assets/images/portfolio/animated/video10.mp4" alt="Video 10">
+                    <img src="./assets/images/portfolio/animated/sc11.webp" data-video="assets/images/portfolio/animated/video11.mp4" alt="Video 11">
+                    <img src="./assets/images/portfolio/animated/sc12.webp" data-video="assets/images/portfolio/animated/video12.mp4" alt="Video 12">
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    </div>
+</div>
+    </div>
+</section>
+<!--  -->
