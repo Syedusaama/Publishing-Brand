6652f76dd9f91a1c341ab474222c28491b76447b
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-06-24 15:23:21 +0500

    update

--- components/reviews.php
+++ components/reviews.php
@@ -14,11 +14,8 @@
 			</div>
 
 			<div class="row pt-5 align-items-center">
-			<div class="col-sm-6">
-					<!-- <div class="review__img">
-						<img src="assets/images/review-left-img-01.png" class="img-responsive" alt="Review">
-					</div> -->
-					<div class="revieew__box review_slider">
+			<div class="col-md-12 col-sm-6">
+					<!-- <div class="revieew__box review_slider">
 						<div class="review__sec__item">
 							<div class="img-responsive-books">
 								<img src="assets/images/review-left-img-01.png" class="img-responsivebooks" alt="Review">
@@ -96,17 +93,207 @@
 						</div>
 					</div>
 				</div>
+			</div> -->
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+		  <div class="carousel-item active">
+		<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-01.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>Global Publishings exceeded my expectations with their publishing service. The publisher assigned to my project not only understood my vision but also introduced me to other platforms. </p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">Wanda F. Wells</span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		  <div class="carousel-item">
+			<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-02.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>The editing and formatting services provided by Global Publishings were top-notch. The team demonstrated professionalism and efficiency throughout the process. My manuscript was transformed into a polished work of art, ready for publication. Highly recommended!</p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">Ken Joslin</span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		  <div class="carousel-item">
+			<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-03.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>Choosing Global Publishings for book marketing was a game-changer for me. Their strategic approach, coupled with targeted campaigns, significantly boosted the visibility of my book. I saw a noticeable increase in sales and reader engagement. Grateful for their expertise!</p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">JR Schaur</span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		</div>
+		<!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Next</span>
+		</a> -->
+	  </div>
 
 
-
-			</div>
 
 		</div>
 
 		<div class="review-vector1">
-			
 			<img src="assets/images/vector-img-02.webp" class="img-responsive" alt="vector">
 		</div>
 	</section>
 
 <!-- Review Section End -->
+
+
+<section>
+	<div class="container">
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+		  <div class="carousel-item active">
+		<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-01.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>Global Publishings exceeded my expectations with their publishing service. The publisher assigned to my project not only understood my vision but also introduced me to other platforms. </p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">Eva James </span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		  <div class="carousel-item">
+			<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-01.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>Global Publishings exceeded my expectations with their publishing service. The publisher assigned to my project not only understood my vision but also introduced me to other platforms. </p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">Eva James </span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		  <div class="carousel-item">
+			<div class="row align-items-center">
+				<div class="col-md-6">
+					<img class="d-block w-100" src="assets/images/review-left-img-01.png" alt="First slide">
+				</div>
+				<div class="col-md-6">
+					<div class="review__sec__item">
+						<div class="rating__review">
+							<img src="assets/images/star-rating.svg" class="img-responsive" alt="Rating">
+						</div>
+						<div class="review__content">
+							<p>Global Publishings exceeded my expectations with their publishing service. The publisher assigned to my project not only understood my vision but also introduced me to other platforms. </p>
+						</div>
+		
+						<div class="review_auth">
+							<div class="review__img">
+								<img srcset="assets/images/review-img-37.png" class="img-responsive" alt="review">
+							</div>
+							<div class="review__auth">
+								<span class="auth__name">Eva James </span>
+								<span class="auth__des">Author</span>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		  </div>
+		</div>
+		<!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		  <span class="sr-only">Next</span>
+		</a> -->
+	  </div>
+	</div>
+	</section>
